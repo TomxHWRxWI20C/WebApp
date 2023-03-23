1067934e2c2e4fb5ab11f45d7aa1342f185f99ef
TomxHWRxWI20C <84907054+TomxHWRxWI20C@users.noreply.github.com>
2023-03-23 11:33:42 +0100

    add transition animation

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 
-  
 function handleTouch() {
-    fetch('https://api.quotable.io/random')
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error('Network response was not OK');
-      }
-      return response.json();
-    })
-    .then((json) => {
+
+     // fade out old quote
+    $( "#quote, #author" ).fadeOut("slow").promise().done(function() {
+
+       // fetch new quote
+      fetch('https://api.quotable.io/random')
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Network response was not OK');
+        }
+        return response.json();
+      })
+      .then((json) => {
         const quote = document.getElementById('quote');
         const author = document.getElementById('author');
         if(quote){
@@ -17,10 +21,17 @@ function handleTouch() {
         if(author){
           author.innerText = json.author;
         }
-    })
-    .catch((error) => {
-      console.error('There has been a problem with your fetch operation:', error);
-    });    
+      })
+      .catch((error) => {
+        console.error('There has been a problem with your fetch operation:', error);
+      }); 
+      
+      // fade in new quote
+      $( "#quote, #author" ).delay(250).fadeIn( "slow", function() {
+      });
+
+    });
+   
 }
 
 
